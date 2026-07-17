e190baa635f40826befc84321719243092ee849f
Muhanad Zahra <muhanad@glencoesoftware.com>
2026-07-17 16:32:00 +0100

    fix regression issues

--- omero/import_scripts/Populate_Metadata.py
+++ omero/import_scripts/Populate_Metadata.py
@@ -34,9 +34,12 @@ from omero.util.populate_roi import DownloadingOriginalFileProvider
 
 try:
     # Hopefully this will import
-    # https://github.com/ome/omero-metadata/blob/v0.3.1/src/populate_metadata.py
+    # Automatic header detection requires omero-metadata >= 0.11.0.
     from omero_metadata.populate import ParsingContext
-    from omero_metadata.cli import MetadataControl
+    try:
+        from omero_metadata.cli import MetadataControl
+    except ImportError:
+        MetadataControl = None
     OBJECT_TYPES = (
         'Plate',
         'Screen',
@@ -48,6 +51,7 @@ try:
 
 except ImportError:
     from omero.util.populate_metadata import ParsingContext
+    MetadataControl = None
     OBJECT_TYPES = (
         'Plate',
         'Screen',
@@ -131,10 +135,19 @@ def populate_metadata(client, conn, script_params):
         return "Please upgrade omero-py to 5.9.1 or later"
     objecti = getattr(omero.model, data_type + 'I')
     omero_object = objecti(int(object_id), False)
-    header_type = MetadataControl.detect_headers(temp_name,
-                                                 keep_default_na=True)
-    ctx = ParsingContext(client, omero_object, "", column_types=header_type,
-                         allow_nan=allow_nan)
+    if MetadataControl is not None and \
+            hasattr(MetadataControl, "detect_headers"):
+        header_type = None
+        with open(temp_name, 'rt', encoding='utf-8-sig') as csv_file:
+            first_row = csv_file.readline()
+        if "# header" not in first_row:
+            header_type = MetadataControl.detect_headers(
+                temp_name, keep_default_na=allow_nan)
+        ctx = ParsingContext(
+            client, omero_object, "", column_types=header_type,
+            allow_nan=allow_nan)
+    else:
+        ctx = ParsingContext(client, omero_object, "")
 
     try:
         if hasattr(ctx, "parse_from_handle"):
